7c175836539138bf2571b525c577155a2b74ce89
MSearle00 <69463861+MSearle00@users.noreply.github.com>
2020-09-01 09:35:31 +0100

    added validation and reset

--- quiz.js
+++ quiz.js
@@ -15,33 +15,62 @@ function getQuestion1() {
 }
 
 function getQuestion2() {
-    document.getElementById("question1").style.display = "none";
-    document.getElementById("question2").style.display = "inline-block";
+    var answers = document.getElementsByName("question1")
+    if (answers[0].checked || answers[1].checked || answers[2].checked)
+      {document.getElementById("question1").style.display = "none";
+      document.getElementById("question2").style.display = "inline-block";
+    }
+    else alert('Please choose an answer')
 }
 
   function getQuestion3() {
-    document.getElementById("question2").style.display = "none";
-    document.getElementById("question3").style.display = "inline-block";
+    var answers = document.getElementsByName("question2")
+    if (answers[0].checked || answers[1].checked || answers[2].checked)
+      {document.getElementById("question2").style.display = "none";
+      document.getElementById("question3").style.display = "inline-block";
+    }
+    else alert('Please choose an answer')   
   }
 
   function getQuestion4() {
-    document.getElementById("question3").style.display = "none";
-    document.getElementById("question4").style.display = "inline-block";
+    var answers = document.getElementsByName("question3")
+    if (answers[0].checked || answers[1].checked || answers[2].checked)
+      {document.getElementById("question3").style.display = "none";
+      document.getElementById("question4").style.display = "inline-block";
+    }
+    else alert('Please choose an answer')   
   }
 
   function getQuestion5() {
-    document.getElementById("question4").style.display = "none";
-    document.getElementById("question5").style.display = "inline-block";
+    var answers = document.getElementsByName("question4")
+    if (answers[0].checked || answers[1].checked || answers[2].checked)
+      {document.getElementById("question4").style.display = "none";
+      document.getElementById("question5").style.display = "inline-block";
+    }
+    else alert('Please choose an answer')   
   }
   function getQuestion6() {
-    document.getElementById("question5").style.display = "none";
-    document.getElementById("question6").style.display = "inline-block";
+    var answers = document.getElementsByName("question5")
+    if (answers[0].checked || answers[1].checked || answers[2].checked)
+      {document.getElementById("question5").style.display = "none";
+      document.getElementById("question6").style.display = "inline-block";
+    }
+    else alert('Please choose an answer')    
   }
   function getResults() {
-    document.getElementById("question6").style.display = "none";
-    document.getElementById("results").style.display = "inline-block";
+    caclulateResults()
+    var answers = document.getElementsByName("question6")
+    if (answers[0].checked || answers[1].checked || answers[2].checked)
+      {document.getElementById("question6").style.display = "none";
+      document.getElementById("results").style.display = "inline-block";
+    }
+    else alert('Please choose an answer')
+  }
+  function caclulateResults(){
+      
   }
   function restart() {
     document.getElementById("results").style.display = "none";
     document.getElementById("introCard").style.display = "inline-block";
-  }
+    document.getElementById("quizform").reset();
+  }      
